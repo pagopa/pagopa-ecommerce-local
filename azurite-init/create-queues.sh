fdc941fdb660ade579fb1f1341044cf1beb79c86
Pietro Tota <pietro.tota@pagopa.it>
2023-04-11 15:32:43 +0200

    fix: script log

--- azurite-init/create-queues.sh
+++ azurite-init/create-queues.sh
@@ -6,7 +6,7 @@ do
     queues=$(cat $f | sed -nr "s/^.*=(pagopa-ecommerce-.*-queue)$/\1/p")
     for queue in $queues
         do
-            echo "Found queue info [$f] file -> $queue, creating queue"
+            echo "Found queue into [$f] env file -> $queue, creating queue"
             az storage queue create -n $queue --connection-string='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;QueueEndpoint=http://storage:10001/devstoreaccount1'
         done
 done
