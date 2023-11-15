75aaf6423e8a113f128c21629ce17dced4c3a1ce
Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
2023-11-15 15:44:00 +0100

    fix(conf): add event dispatcher env properties (#107)
    * fix(conf): add event dispatcher env properties
    
    * fix: add header logs for npg mock

--- npg-mock/middleware.js
+++ npg-mock/middleware.js
@@ -1,4 +1,10 @@
 module.exports = function (req, res, next) {
+    console.log(`${new Date().toISOString()} - Received request: ${req.method} ${req.path}
+     Headers: 
+     ${JSON.stringify(req.headers)}
+     Body:  
+     ${JSON.stringify(req.body)}
+     `);
     if (req.method === 'POST') {
       // Converts POST to GET
       req.method = 'GET'
