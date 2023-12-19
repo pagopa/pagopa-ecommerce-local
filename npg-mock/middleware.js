83b6e33def20e3a75f813e9a374cd7102ea9b9a7
Pietro Tota <pietro.tota@pagopa.it>
2023-12-19 20:11:48 +0100

    feat: add order build npg mock for CARDS and APM

--- npg-mock/middleware.js
+++ npg-mock/middleware.js
@@ -1,14 +1,29 @@
 module.exports = function (req, res, next) {
-    console.log(`${new Date().toISOString()} - Received request: ${req.method} ${req.path}
-     Headers: 
-     ${JSON.stringify(req.headers)}
-     Body:  
-     ${JSON.stringify(req.body)}
-     `);
-    if (req.method === 'POST') {
-      // Converts POST to GET
-      req.method = 'GET'
-    }
-    // Continue to JSON Server router
-    next()
+  const db = req.app.db
+  console.log(`${new Date().toISOString()} - Received request: ${req.method} ${req.path}
+  Headers: 
+  ${JSON.stringify(req.headers)}
+  Body:  
+  ${JSON.stringify(req.body)}
+  `);
+  if (req.method === 'POST') {
+    // Converts POST to GET
+    req.method = 'GET'
   }
+  const requestPath = req.path.toString();
+  const requestBody = req.body;
+  if (requestPath.includes("build")) {
+    //handle build data request changing response based on input paymentService
+    const paymentService = requestBody.paymentSession.paymentService;
+    console.log(`Received build request for payment session: ${paymentService}`);
+    if (paymentService == "CARDS") {
+      res.json(db.get("buildCards"))
+    }
+    else {
+      res.json(db.get("buildApm"))
+    }
+  } else {
+    // Continue to default JSON Server router
+    next();
+  }
+}
