3a7f123395517cf38a09cb9d44a8a246a4564918
ciuffagianluca <113357981+ciuffagianluca@users.noreply.github.com>
2024-09-05 13:44:55 +0200

    [CHK-3172] remove xpay vpos refs (#172)
    * remove xpay vpos refs
    
    * remove pgs from docker compose
    
    * Update .env
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    
    * Update .env
    
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
    Co-authored-by: Gianluca Ciuffa <gianlucaciuffa@MacBook-Pro-di-Gianluca.local>
    Co-authored-by: Pietro Tota <115724836+pietro-tota@users.noreply.github.com>

--- pgs-mock/middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res, next) {
-    if (req.method === 'POST') {
-      // Converts POST to GET
-      req.method = 'GET'
-    }
-    // Continue to JSON Server router
-    next()
-  }
