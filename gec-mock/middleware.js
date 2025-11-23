adce7a37e8af1831dfabe86ed53ae6dd700cafaa
Simone infante <52280205+infantesimone@users.noreply.github.com>
2025-11-23 10:14:15 +0100

    feat: add  api test for ecommerce payment methods handler (#246)

--- gec-mock/middleware.js
+++ gec-mock/middleware.js
@@ -5,10 +5,30 @@ module.exports = function (req, res, next) {
   Body:  
   ${JSON.stringify(req.body)}
   `);
-    if (req.method === 'POST') {
-      // Converts POST to GET
-      req.method = 'GET'
-    }
-    // Continue to JSON Server router
-    next()
+
+  // Converts POST to GET
+  if (req.method === 'POST') {
+    req.method = 'GET';
   }
+
+  // --- START PAYMENT-METHODS for IO and for CHECKOUT ---
+  const methodByIdRegex = /^\/payment-methods\/([^\/]+)$/;
+  const match = req.path.match(methodByIdRegex);
+
+  if (match) {
+    const id = match[1];  
+    console.log("Intercepted payment method ID:", id);
+    const db = require('./gec-server.json');
+    if (id === "378d0b4f-8b69-46b0-8215-07785fe1aad4") {
+      return res.json(db.methodByIdForCheckout);
+    }
+    if (id === "77cb0c8d-5aec-4b68-835f-eef824e83c4e") {
+      return res.json(db.methodByIdForIO);
+    }
+    return res.status(404).json({ error: "Payment method not found" });
+  }
+  // --- END PAYMENT-METHODS for IO and for CHECKOUT ---
+  
+  // Continue to JSON Server router
+  next()
+}
